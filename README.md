9fd3b99fe0b66e11908535939633192678fa916f
Sami Nassim <sami.bououdine@gmail.com>
2024-01-20 18:49:52 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Mon Vieux Grimoire - Java edition
+
+J'ai décidé de réécrire le backend du projet 7 de ma formation OpenClassrooms (NodeJS / ExpressJS) entièrement en Java. </br>
+<i>More coming soon...</i>
---
+++ src/main/java/com/saminassim/mvgjava/MvgJavaApplication.java
@@ -0,0 +1,13 @@
+package com.saminassim.mvgjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvgJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MvgJavaApplication.class, args);
+    }
+
+}
