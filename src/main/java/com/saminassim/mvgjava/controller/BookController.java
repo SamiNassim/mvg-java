72288a540666262eacdceb48e5b365233e9cafa6
Sami Nassim <sami.bououdine@gmail.com>
2024-01-31 19:25:16 +0100

    feat: Added modifyBook endpoint

--- src/main/java/com/saminassim/mvgjava/controller/BookController.java
+++ src/main/java/com/saminassim/mvgjava/controller/BookController.java
@@ -2,9 +2,11 @@ package com.saminassim.mvgjava.controller;
 
 import com.saminassim.mvgjava.dto.BookRatingRequest;
 import com.saminassim.mvgjava.dto.BookRequest;
+import com.saminassim.mvgjava.dto.ModifyBookRequest;
 import com.saminassim.mvgjava.entity.Book;
 import com.saminassim.mvgjava.exception.BookAlreadyRatedException;
 import com.saminassim.mvgjava.exception.BookCannotBeDeletedException;
+import com.saminassim.mvgjava.exception.BookCannotBeModifiedException;
 import com.saminassim.mvgjava.service.BookService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -42,6 +44,18 @@ public class BookController {
     @GetMapping("/{id}")
     public Optional<Book> getOneBook(@PathVariable UUID id) {
         return bookService.getOneBook(id);
+    }
+
+    @PutMapping("/{id}")
+    @PreAuthorize("hasAuthority('USER')")
+    public ResponseEntity<?> modifyBook(@PathVariable UUID id, @RequestBody ModifyBookRequest modifyBookRequest) {
+        try {
+            bookService.modifyBook(id, modifyBookRequest);
+            return ResponseEntity.ok().build();
+        } catch (BookCannotBeModifiedException e) {
+            return ResponseEntity.status(401)
+                    .body(e.getMessage());
+        }
     }
 
     @DeleteMapping("/{id}")
